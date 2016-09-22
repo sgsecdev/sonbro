1c9883d71910b2bf7f502ddfa391290f895eebff
sanggyun <sgsecdev@gmail.com>
2016-09-23 03:15:57 +0900

    아따 고마 java file만 올리야겠네...

--- sonbro/src/sonbro/HelloWorld.java
+++ sonbro/src/sonbro/HelloWorld.java
@@ -2,9 +2,6 @@ package sonbro;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("카아아아악 퉷!");
-		System.out.println("로퍼하나사고 친구 만나고오니까 AM 02:00였는데");
-		System.out.println("git하고 sourcetree 설치하고 전자정부프레임워크 설치하니까 AM 03:00이네 ");
-		System.out.println("난 약속을 지켰음 그럼 오늘은 이만");
+		System.out.println("Hello World");
 	}
 }
